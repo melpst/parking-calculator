dfb552f4743855fc11130eb701844be1fb1f583c
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-03-12 23:49:10 +0700

    edit 'parkingTime' to 'entryTime' and can getTime of entryTime, currentTime

--- routes/mall.js
+++ routes/mall.js
@@ -14,17 +14,33 @@ router.get('/parking', (req,res) => {
 })
 
 router.get('/:id/checkprice', (req, res) => {
-	let fee = 0
-	res.send('Total fee = '+fee)
+	Parking.findOne({mallId: req.params.id})
+	.then((data) => {
+		if(data === undefined){
+			res.send('Parking lot hasn\'t been occupied yet')
+		}
+		else{
+			const entryTime = data.entryTime.getTime()
+			const currentTime = new Date().getTime()
+			console.log('entry time : ' + entryTime)
+			console.log('current time : ' + currentTime)
+	
+			const duration = (currentTime - entryTime)/(1000*60)
+			console.log('duration : ' + duration)
+	
+			let fee = 0
+			res.send('Total fee = '+duration)
+		}
+	})
 })
 
 router.post('/:id/checkin', (req, res) => {
 	Parking.findOne({mallId: req.params.id})
 	.then((data) =>{
-		if(data == undefined){
+		if(data === undefined){
 			const parking = new Parking()
 			parking.mallId = req.params.id
-			parking.parkingTime = req.body.entryTime
+			parking.entryTime = req.body.entryTime
 			parking.save()
 			.then((data) => res.send('Check-in completed'))
 		}
@@ -37,7 +53,7 @@ router.post('/:id/checkin', (req, res) => {
 router.post('/price', (req, res) => {
 	Mall.findOne({id: req.body.id})
 	.then((data) => {
-		if(data == undefined){
+		if(data === undefined){
 			const price = new Mall (req.body)
 			price.save()
 			.then((data) => res.send(data))
@@ -52,7 +68,7 @@ router.post('/price', (req, res) => {
 router.delete('/:id/checkout', (req, res) => {
 	Parking.findOne({mallId: req.params.id})
 	.then((data) =>{
-		if(data == undefined){
+		if(data === undefined){
 			res.send('Parking lot hasn\'t been occupied yet')
 		}
 		else{
