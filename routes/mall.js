50216ba67fba05873eb37331cb8995cb63e1e4fd
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-03-12 21:22:05 +0700

    '/mall' will show all mall's fee and '/mall/price' can add mall's fee

--- routes/mall.js
+++ routes/mall.js
@@ -1,28 +1,39 @@
 const {Router} = require('express')
+const {Mall, Parking} = require('../models')
 
 const router = Router()
 
-router.get('/', (req,res) => res.send('hello, world'))
+router.get('/', (req,res) => {
+	Mall.find()
+	.then((data) => res.send(data))
+})
 
 router.get('/:id/checkprice', (req, res) => {
 	let fee = 0
 	res.send('Total fee = '+fee)
 })
 
-router.get('/:id/price', (req, res) => {
-	res.send('list of mall\'s price')
-})
-
 router.post('/:id/checkin', (req, res) => {
 	res.send('Check-in completed')
 })
 
-router.post('/:id/checkout', (req, res) => {
-	res.send('Check-out completed')
+router.post('/price', (req, res) => {
+	Mall.findOne({id: req.body.id})
+	.then((data) => {
+		if(data == undefined){
+			const price = new Mall (req.body)
+			price.save()
+			.then((data) => res.send(data))
+		}
+		else{
+			res.send('This mall has been added already')
+		}
+		
+	})
 })
 
-router.post('/:id/price', (req, res) => {
-	res.send(req.body)
+router.delete('/:id/checkout', (req, res) => {
+	res.send('Check-out completed')
 })
 
 module.exports = router
