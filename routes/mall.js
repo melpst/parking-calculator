e466a713313c79307926ee937566c3f145d66e26
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-03-12 14:14:58 +0700

    create apis in mall - '/:id/checkprice', '/:id/checkin', '/:id/checkout'

--- routes/mall.js
+++ routes/mall.js
@@ -4,4 +4,17 @@ const router = Router()
 
 router.get('/', (req,res) => res.send('hello, world'))
 
+router.get('/:id/checkprice', (req, res) => {
+	let fee = 0
+	res.send('Total fee = '+fee)
+})
+
+router.post('/:id/checkin', (req, res) => {
+	res.send('Check-in completed')
+})
+
+router.post('/:id/checkout', (req, res) => {
+	res.send('Check-out completed')
+})
+
 module.exports = router
