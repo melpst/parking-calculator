dc5e321d9ee528f981d9cdc4215bb057ab59263e
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-03-12 17:02:03 +0700

    add get, post apis - '/:id/price'

--- routes/mall.js
+++ routes/mall.js
@@ -9,12 +9,20 @@ router.get('/:id/checkprice', (req, res) => {
 	res.send('Total fee = '+fee)
 })
 
+router.get('/:id/price', (req, res) => {
+	res.send('list of mall\'s price')
+})
+
 router.post('/:id/checkin', (req, res) => {
 	res.send('Check-in completed')
 })
 
 router.post('/:id/checkout', (req, res) => {
 	res.send('Check-out completed')
+})
+
+router.post('/:id/price', (req, res) => {
+	res.send(req.body)
 })
 
 module.exports = router
