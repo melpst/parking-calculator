8b0e04307c440d66a143e4f5dc1f227b5cc3b9dd
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-03-13 02:55:38 +0700

    edit codes

--- price_ctw.py
+++ price_ctw.py
@@ -3,9 +3,10 @@ fee = 0
 for i in range(30,1441,30):
 	if i<=(0.5*60):
 		fee += 0
-	elif i<=(5*60):
-	  if i == 60 or i%30 == 0 and i%60 != 0:
-		  fee += 20
 	else:
-		fee += 50
+		if i%30 == 0 and i%60 != 0 or i == 60:
+			if i<=(5*60):
+			  fee += 20
+			else:
+				fee += 50
 	print '"%d": %d,' %(i,fee)
